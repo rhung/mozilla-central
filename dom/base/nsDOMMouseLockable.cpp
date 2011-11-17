8f4e4cccead606f6d0df8fa492a913d348280645
Raymond Hung <hung.raymond@gmail.com>
2011-11-16 22:53:02 -0500

    Changed PR_TRUE to PR_FALSE when using the Unlock method

--- dom/base/nsDOMMouseLockable.cpp
+++ dom/base/nsDOMMouseLockable.cpp
@@ -63,7 +63,7 @@ nsDOMMouseLockable::~nsDOMMouseLockable()
 /* void unlock (); */
 NS_IMETHODIMP nsDOMMouseLockable::Unlock()
 {
-  mIsLocked = PR_TRUE;
+  mIsLocked = PR_FALSE;
   return NS_OK;
 }
 
