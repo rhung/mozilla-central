c7f80a3fa433c0ea53f4f04d68f19163e38cbbe4
David Humphrey <david.humphrey@senecac.on.ca>
2011-12-01 17:42:15 -0500

    Remove old changes to nsIDOMWindow for setting mouse location.

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -4099,28 +4099,6 @@ nsGlobalWindow::SetScreenY(PRInt32 aScreenY)
   return NS_OK;
 }
 
-NS_IMETHODIMP
-nsGlobalWindow::GetLastScreenX(PRInt32* aScreenX)
-{
-  *aScreenX = mLastScreenPos.x;
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsGlobalWindow::GetLastScreenY(PRInt32* aScreenY)
-{
-  *aScreenY = mLastScreenPos.y;
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsGlobalWindow::SaveScreenPosition(PRInt32 x, PRInt32 y)
-{
-  mLastScreenPos.x = x;
-  mLastScreenPos.y = y;
-  return NS_OK;
-}
-
 // NOTE: Arguments to this function should have values scaled to
 // CSS pixels, not device pixels.
 nsresult
